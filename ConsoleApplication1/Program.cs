d85bc821a9916c7b87208388bc76071d617d8dcf
Unknown <jrohweller13@apu.edu>
2017-07-19 10:57:16 -0700

    Can see serial numbers! finally!

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.IO;
+using System.Text;
 
 namespace RSPSample1
 {
@@ -47,15 +48,16 @@ namespace RSPSample1
 
  //all of the above are used with getting data from device. (came with C# program initiallly.) below is from us.
  
+//keyword
  //used with getDevices
         public unsafe struct mir_sdr_DeviceT
         {
-            public char* SerNo;
-            public char* DevNm;
+            public IntPtr SerNo;
+            public IntPtr DevNm;
             public byte hwVer;
             public byte devAvail;
 
-            public mir_sdr_DeviceT(char* p1, char* p2, byte p3, byte p4)
+            public mir_sdr_DeviceT(IntPtr p1, IntPtr p2, byte p3, byte p4)
             {
                 SerNo = p1;
                 DevNm = p2;
@@ -64,7 +66,23 @@ namespace RSPSample1
             }
         }
 
- //used to set band type (with what function idk) (not implemented yet)
+        public unsafe struct mir_sdr_DeviceTwo
+        {
+            public string SerNo;
+            public string DevNm;
+            public byte hwVer;
+            public byte devAvail;
+
+            public mir_sdr_DeviceTwo(string p1, string p2, byte p3, byte p4)
+            {
+                SerNo = p1;
+                DevNm = p2;
+                hwVer = p3;
+                devAvail = p4;
+            }
+        }
+
+        //used to set band type (with what function idk) (not implemented yet)
         private enum mir_sdr_RSPII_BandT
         {
             mir_sdr_RSPII_BAND_UNKNOWN = 0,
@@ -109,6 +127,7 @@ namespace RSPSample1
 
 
 //multiple devices (our implementations)
+//keyword 
  //mir_sdr_GetDevices
         [DllImport("C:\\Program Files\\SDRplay\\API\\x86\\mir_sdr_api.dll")]
         private static extern mir_sdr_ErrT mir_sdr_GetDevices(mir_sdr_DeviceT[] devices, ref uint numDevs, uint maxDevs);
@@ -146,31 +165,58 @@ namespace RSPSample1
             {
             mir_sdr_ErrT r;                                                             //ErrT object initialized (r is used to check program API success)
             
-            mir_sdr_DeviceT[] ourDevices;                                               //array of Device structs (look at mir_sdr_DeviceT above)
-            ourDevices = new mir_sdr_DeviceT[numberOfSDRs];                             //expect correct # of devices from what we told it.
+            //keyword
+            string test1= "1234567890";
+            string test2 = "0";
+            byte test3 = 0;
+            byte test4 = 0;
+            mir_sdr_DeviceTwo firstDevice = new mir_sdr_DeviceTwo(test1, test2, test3, test4);
+            mir_sdr_DeviceTwo secondDevice = new mir_sdr_DeviceTwo(test1, test2, test3, test4);
+            
+            IntPtr T1 = Marshal.StringToHGlobalUni(test1 );
+            IntPtr T2 = Marshal.StringToHGlobalUni(test2 );
+            mir_sdr_DeviceT firstDevice1 = new mir_sdr_DeviceT(T1, T2, test3, test4);
+            mir_sdr_DeviceT secondDevice1 = new mir_sdr_DeviceT(T1, T2, test3, test4);
+            
 
+            //mir_sdr_DeviceT[] ourDevices;                                               //array of Device structs (look at mir_sdr_DeviceT above)
+            //ourDevices = new mir_sdr_DeviceT[numberOfSDRs];                             //expect correct # of devices from what we told it.
+            //mir_sdr_DeviceTwo[]ourDevices2 = new mir_sdr_DeviceTwo[] { firstDevice, secondDevice};
+            //Console.WriteLine(ourDevices2[z].SerNo);
+
+            mir_sdr_DeviceT[]ourDevices3 = new mir_sdr_DeviceT[] { firstDevice1, secondDevice1};
+            
+
+            //keyword
             uint numberDevs=1;                                                          //is later changed to # of devices found by API when function is called.
             uint maximumDevs=8;                                                         //maximum number of devices we want.
-            r = mir_sdr_GetDevices( ourDevices, ref numberDevs, maximumDevs);           //fcn: takes in array of device structs, a var to store # of devices found,
+            r = mir_sdr_GetDevices( ourDevices3, ref numberDevs, maximumDevs);           //fcn: takes in array of device structs, a var to store # of devices found,
             if (r != mir_sdr_ErrT.mir_sdr_Success)                                      //and maximum devices we want. If it worked, r= success.
             {
               Console.WriteLine("Failed to get the IDs of the devices.");               //otherwise, print our failure.
               
             }
 
+            Console.WriteLine(ourDevices3[z].SerNo);
+            //keyword
+            //string s = Marshal.PtrToStringAnsi((IntPtr)ourDevices[z].DevNm);
+            //string s = new string(ourDevices[z].SerNo);
+            //Console.WriteLine(s);
+            //Console.WriteLine(ourDevices[z].devAvail); 
+            //Console.WriteLine(ourDevices[z].SerNo);
+            //Console.WriteLine(ourDevices[z].SerNo.ToString());
+            
+
+           //keyword
+           string a= "1";
+           
+            
             //test with own devices
-            //char test1= "0;
-            //char test2 = 0;
+            //string test1= "1234567890";
+            //string test2 = "0";
             //byte test3 = 0;
             //byte test4 = 0;
-            //mir_sdr_DeviceT firstDevice = new mir_sdr_DeviceT(ref test1, ref test2, test3, test4);
-            
-            //
-            //unsafe
-            //    {
-            //Console.WriteLine(*ourDevices[z].SerNo);
-            //    }
-
+            //mir_sdr_DeviceT firstDevice = new mir_sdr_DeviceT(test1, test2, test3, test4);
 
             uint myIdx=Convert.ToUInt32(z);                                             //convert for loop counter (z) to uint, store in myIdx
                 r = mir_sdr_SetDeviceIdx(myIdx);                                        //fcn: takes in which device you want (myIdx), -we use # of devices in for loop
