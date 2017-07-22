f7eb61b7d4a94a36b0bcf97c474fae139f9ab500
Unknown <jrohweller13@apu.edu>
2017-07-22 13:06:43 -0700

    Tested More

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -68,12 +68,12 @@ namespace RSPSample1
 
         public unsafe struct mir_sdr_DeviceTwo
         {
-            public string SerNo;
-            public string DevNm;
+            public byte[] SerNo;
+            public byte[] DevNm;
             public byte hwVer;
             public byte devAvail;
 
-            public mir_sdr_DeviceTwo(string p1, string p2, byte p3, byte p4)
+            public mir_sdr_DeviceTwo(byte[] p1, byte[] p2, byte p3, byte p4)
             {
                 SerNo = p1;
                 DevNm = p2;
@@ -130,7 +130,7 @@ namespace RSPSample1
 //keyword 
  //mir_sdr_GetDevices
         [DllImport("C:\\Program Files\\SDRplay\\API\\x86\\mir_sdr_api.dll")]
-        private unsafe static extern mir_sdr_ErrT mir_sdr_GetDevices(mir_sdr_DeviceT*[] devices, ref uint numDevs, uint maxDevs);
+        private unsafe static extern mir_sdr_ErrT mir_sdr_GetDevices(mir_sdr_DeviceTwo[] devices, ref uint numDevs, uint maxDevs);
 
  //mir_sdr_SetDeviceIdx
         [DllImport("C:\\Program Files\\SDRplay\\API\\x86\\mir_sdr_api.dll")]
@@ -160,28 +160,28 @@ namespace RSPSample1
             const int DEFAULT_BUF_LENGTH = (336 * 2);                                   //default buffer length
 
             //for loop begins
-            int numberOfSDRs = 2;                                                       //only change this line to add more SDRs
+            int numberOfSDRs = 1;                                                       //only change this line to add more SDRs
             for (int z = 0; z < numberOfSDRs; z++)                                      //for # of SDRs, read and output a file
             {
             mir_sdr_ErrT r;                                                             //ErrT object initialized (r is used to check program API success)
             
             //keyword
-            string test1= "1234567890";
-            string test2 = "0";
-            byte test3 = 0;
-            byte test4 = 0;
-            mir_sdr_DeviceTwo firstDevice = new mir_sdr_DeviceTwo(test1, test2, test3, test4);
-            mir_sdr_DeviceTwo secondDevice = new mir_sdr_DeviceTwo(test1, test2, test3, test4);
+            //string test1= "1234567890";
+            //string test2 = "0";
+            //byte test3 = 0;
+            //byte test4 = 0;
+            //mir_sdr_DeviceTwo firstDevice = new mir_sdr_DeviceTwo(test1, test2, test3, test4);
+            //mir_sdr_DeviceTwo secondDevice = new mir_sdr_DeviceTwo(test1, test2, test3, test4);
             
-            IntPtr T1 = Marshal.StringToHGlobalUni(test1 );
-            IntPtr T2 = Marshal.StringToHGlobalUni(test2 );
+            //IntPtr T1 = Marshal.StringToHGlobalUni(test1 );
+            //IntPtr T2 = Marshal.StringToHGlobalUni(test2 );
             //mir_sdr_DeviceT firstDevice1 = new mir_sdr_DeviceT(T1, T2, test3, test4);
             //mir_sdr_DeviceT secondDevice1 = new mir_sdr_DeviceT(T1, T2, test3, test4);
             
 
-            mir_sdr_DeviceT*[] ourDevices;                                               //array of Device structs (look at mir_sdr_DeviceT above)
-            ourDevices = new mir_sdr_DeviceT*[numberOfSDRs];                             //expect correct # of devices from what we told it.
-            mir_sdr_DeviceTwo[]ourDevices2 = new mir_sdr_DeviceTwo[] { firstDevice, secondDevice};
+            mir_sdr_DeviceTwo[] ourDevices;                                               //array of Device structs (look at mir_sdr_DeviceT above)
+            ourDevices = new mir_sdr_DeviceTwo[numberOfSDRs];                             //expect correct # of devices from what we told it.
+            //mir_sdr_DeviceTwo[]ourDevices2 = new mir_sdr_DeviceTwo[] { firstDevice, secondDevice};
             //Console.WriteLine(ourDevices2[z].SerNo);
 
             //mir_sdr_DeviceT[]ourDevices3 = new mir_sdr_DeviceT[] { firstDevice1, secondDevice1};
@@ -200,8 +200,8 @@ namespace RSPSample1
             //Console.WriteLine(ourDevices3[z].SerNo);
             //keyword
             //string s = Marshal.PtrToStringAnsi((IntPtr)ourDevices[z].DevNm);
-            string s = new string(ourDevices[z]->SerNo);
-            Console.WriteLine(s);
+            //string s = new string(ourDevices[z].SerNo);
+            Console.WriteLine(ourDevices[z].SerNo[0]);
             //Console.WriteLine(ourDevices[z].hwVer);
             
             //Console.WriteLine(ourDevices[z].SerNo);
